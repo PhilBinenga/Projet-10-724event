8a2849567f463b23170b3721237e6cd985e73c65
Philemon Binenga <philemonbinenga@gmail.com>
2024-03-07 12:05:23 +0100

    correction slider / date

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,13 +8,16 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  // Changement pour trier les photos de la plus ancienne à la plus récente
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
+    if (byDateDesc) {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0),
       5000
     );
+    };
   };
   useEffect(() => {
     nextCard();
@@ -23,13 +26,12 @@ const Slider = () => {
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
         <>
-          <div
-            key={event.title}
+          <div key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
           >
-            <img src={event.cover} alt="forum" />
+            <img src={event.cover} alt={event.title} />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
@@ -40,12 +42,12 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc.map((bulletPoint, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`${bulletPoint.title}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
                 />
               ))}
             </div>
